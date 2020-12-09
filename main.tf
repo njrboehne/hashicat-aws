728994950145d962c2d05734a12e7b1f1d8f5609
njrboehne <natalie.boehne@gmail.com>
2020-12-09 20:40:59 +0000

    spelled department correctly for real this time

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Dapartment = "devops"
+    Department = "devops"
     Billable = "devops"
   }
 }
